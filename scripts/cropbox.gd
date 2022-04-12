0852af5772c86feaa2f0fe297cffa1eeae7c9a77
Gunnar Jessee <gunnarjessee@gmail.com>
2022-04-12 14:59:45 -0400

    added UI control

---
+++ scripts/cropbox.gd
@@ -0,0 +1,24 @@
+extends Sprite
+
+onready var UI_SELECTOR = load('res://scenes/crop_select.tscn')
+
+enum crops {
+	wheat,
+	
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			create_ui()
+
+# Creates a UI above the the crop in context
+func create_ui():
+	var instance = UI_SELECTOR.instance()
+	instance.pos = get_global_mouse_position()
+	add_child(instance)
