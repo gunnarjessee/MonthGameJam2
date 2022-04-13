0d8b29b51aeb22530b3e60553dbed151539625ff
Gunnar Jessee <gunnarjessee@gmail.com>
2022-04-12 22:00:26 -0400

    added active health and wetness to wheat crops

---
+++ scripts/crophandler.gd
@@ -0,0 +1,40 @@
+extends Node
+
+# This script will handle the growth events of every crop
+
+var health: int = 100
+var wetness: int = 10
+var stage: int = 0
+
+var typeofplant: String = "none"
+
+onready var timer = $Timer
+var timerIntervals: int = 5 # this is in seconds
+
+func set_plant(type):
+	typeofplant = type
+	print('Type of plant has been set too ', typeofplant)
+
+func _ready():
+	timer.start(timerIntervals)
+	timer.set_autostart(false)
+
+
+func _on_Timer_timeout():
+	check_plant_health()
+	timer.start(timerIntervals)
+	
+func check_plant_health():
+	print(health, " checking health of plant wetness: ", wetness)
+	if wetness > 4:
+		stage += 1
+	else:
+		health -= 20
+	
+	if wetness > 0:
+		wetness -= 1
+	
+	if health <= 0:
+		get_parent().select_crop(get_parent().crops.none)
+		queue_free()
+		
